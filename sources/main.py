fae837917c8f1d4734e73a18e7ed5ff189e48d81
tora <bmvdsfy@gmail.com>
2024-08-19 23:31:05 +0900

    ゴミ消し消し

--- sources/main.py
+++ sources/main.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 
 # ルーティング
 
-# login処理です
 @app.route('/', methods=['GET', 'POST'])
 def form():
     # ２回目以降データが送られてきた時の処理です
@@ -23,11 +22,9 @@ def form():
     if request.method == 'GET':
         return render_template('form.html')
 
-# from app import db
 
 if __name__ == "__main__":
     args = get_option()
-    # print(url_for('static',filename='form-style.css') )
 
     if args.host:
         app.run(port=1145, debug=False, host="0.0.0.0")
