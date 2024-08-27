ca32e17bddd2b89306d9e8dc75d754523fe1a92f
tora <bmvdsfy@gmail.com>
2024-08-27 18:14:33 +0900

    ボタンレイアウトテスト

--- app/main.py
+++ app/main.py
@@ -1,24 +1,78 @@
-import time
+# import time
+from typing import TypedDict
 
-from flask import Flask, render_template, request, url_for
-from option_parser import get_option
-from output import KeyStroke
+from flask import Flask, render_template, request
+from option import get_option
+
+# from output import KeyStroke
 
 app = Flask(__name__)
 
-# ルーティング
+class Shortcuts(TypedDict):
+    id: int
+    name: str
+    key: list[str]
 
+shortcuts: Shortcuts = [
+    {
+        "id": 1,
+        "name": "undo",
+        "key": ["ctrl", "z"]
+    },
+    {
+        "id": 2,
+        "name": "redo",
+        "key": ["ctrl", "shift", ]
+    },
+    {
+        "id": 3,
+        "name": "redo",
+        "key": ["ctrl", "shift", ]
+    },
+    {
+        "id": 4,
+        "name": "redo",
+        "key": ["ctrl", "shift", ]
+    },
+    {
+        "id": 5,
+        "name": "redo",
+        "key": ["ctrl", "shift", ]
+    },
+    {
+        "id": 6,
+        "name": "redo",
+        "key": ["ctrl", "shift", ]
+    },
+    {
+        "id": 7,
+        "name": "redo",
+        "key": ["ctrl", "shift", ]
+    },
+    {
+        "id": 8,
+        "name": "redo",
+        "key": ["ctrl", "shift", ]
+    },
+    {
+        "id": 9,
+        "name": "redo",
+        "key": ["ctrl", "shift", ]
+    }
+]
+
+
+
+
+
+# ルーティング
 @app.route('/', methods=['GET', 'POST'])
 def form():
     # ２回目以降データが送られてきた時の処理です
     if request.method == 'POST':
-        # ks = KeyStroke()
-        # ks.nextPage()
-        # time.sleep(1)
-        # ks.prevPage()
         print(request.form.get("shortcut"))
     
-        return render_template('form.html', shortcuts=["ctr+z", "ctrl+y", "alt+f4"])
+        return render_template('form.html', shortcut_lists=shortcuts, column=3, row=3)
     
     if request.method == 'GET':
         return render_template('form.html')
