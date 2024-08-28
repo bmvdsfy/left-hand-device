1ee5fa025082adac78fc0302081c580dfc0177d4
tora <bmvdsfy@gmail.com>
2024-08-28 18:50:23 +0900

    型修正

--- app/keytype.py
+++ app/keytype.py
@@ -2,9 +2,14 @@ from typing import TypedDict
 
 # class Keystroke(TypedDict):
 
-class ShortcutOptionalType(TypedDict):
+class Position(TypedDict):
+    row: int
+    column: int
+
+class ShortcutOptionalType(TypedDict, total=False):
     id: int
     description: str
+    position: Position
 
 class Shortcut(ShortcutOptionalType):
     name: str
