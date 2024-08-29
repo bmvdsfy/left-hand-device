3e0345edf9f6f981efaa8f2422f9670f74dca7cb
tora <bmvdsfy@gmail.com>
2024-08-30 06:02:27 +0900

    定型文の挿入

--- app/output.py
+++ app/output.py
@@ -1,12 +1,30 @@
 # import pyperclip
+import time
+
 import pyautogui
+import pyperclip
 from keytype import Shortcut
 
 
-def key_press(shortcut: Shortcut):
-    for key in shortcut["key"]:
+def proceed_shortcut(shortcut: Shortcut):
+    keys = shortcut["key"]
+    if keys[0] == "TypewriteWithClipboard":
+        pyperclip.copy(keys[1])
+        _key_press(["command", "v"])
+        print("paste done")
+
+    else:
+        _key_press(keys)
+
+
+
+def _key_press(keys: list[str]):
+    # キー入力ショートカット
+    for key in keys:
         print("keyDonwn : " + key)
         pyautogui.keyDown(key)
-    for key in reversed(shortcut["key"]): #逆順
+        time.sleep(0.1)
+    for key in reversed(keys): #逆順
         print("keyUp : " + key)
         pyautogui.keyUp(key)    
+        time.sleep(0.1)
